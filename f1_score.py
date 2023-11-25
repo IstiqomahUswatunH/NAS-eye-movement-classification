a612baa4fd0f668b297f0b010dfdd6c891df6af8
IstiqomahUswatunH <hasanah.10105@mail.ugm.ac.id>
2023-11-25 14:03:49 +0700

    initial commit

---
+++ f1_score.py
@@ -0,0 +1,39 @@
+from keras import backend as K
+import numpy as np
+from keras.callbacks import Callback
+from sklearn.metrics import confusion_matrix, f1_score, precision_score, recall_score
+
+class F1_score(Callback):
+    
+    @staticmethod
+    def categorical_f1_score_for_class(y_true, y_pred, class_i, dtype=None):
+        pred_labels = K.argmax(y_pred, axis=-1)
+        tp = K.sum(y_true[:, :, class_i] * K.cast(K.equal(pred_labels, class_i), 'float32' if not dtype else dtype))
+        all_p_detections = K.sum(K.cast(K.equal(pred_labels, class_i), 'float32' if not dtype else dtype))
+        all_p_true = K.sum(y_true[:, :, class_i])
+    
+        precision = tp / (all_p_detections + K.epsilon())
+        recall = tp / (all_p_true + K.epsilon())
+        f_score = 2 * precision * recall / (precision + recall + K.epsilon())
+        return f_score
+
+    @staticmethod
+    def f1_FIX(y_true, y_pred, dtype=None):
+        return F1_score.categorical_f1_score_for_class(y_true, y_pred, 1, dtype)
+    
+    @staticmethod
+    def f1_SACC(y_true, y_pred, dtype=None):
+        return F1_score.categorical_f1_score_for_class(y_true, y_pred, 2, dtype)
+
+    @staticmethod
+    def f1_SP(y_true, y_pred, dtype=None):
+        return F1_score.categorical_f1_score_for_class(y_true, y_pred, 3, dtype)
+
+    # create function for calculate f1 score macro
+    @staticmethod
+    def f1_macro(y_true, y_pred, dtype=None):
+        f1_FIX_score = F1_score.f1_FIX(y_true, y_pred, dtype)
+        f1_SACC_score = F1_score.f1_SACC(y_true, y_pred, dtype)
+        f1_SP_score = F1_score.f1_SP(y_true, y_pred, dtype)
+        f1_macro = (f1_FIX_score + f1_SACC_score + f1_SP_score) / 3
+        return f1_macro
