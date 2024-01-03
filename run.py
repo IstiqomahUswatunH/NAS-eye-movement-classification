fda805d5f9f1397da4c7c670c1f84f2384d0c8c8
IstiqomahUswatunH <hasanah.10105@mail.ugm.ac.id>
2024-01-03 17:11:30 +0700

    change prep data

--- run.py
+++ run.py
@@ -6,56 +6,40 @@ from model import MODEL
 from CONSTANTS import TOP_N
 import math
 
-def load_data(df, seq_len,ppd_value):
-  """ Load and preprocess data
-
-  Args:
-  - df : dataframe of tubular data (df.iloc[i] == one row)
-  - seq_len : length of one sample/sequence
-
-  returns:
-  - [X, trainy]
-     X      : time series data (num_sample, seq_len, feature) no overlapping,
-              numpy array, rescaled, float32
-     trainy : labels of each sample in the time series data, numpy array
-  -  scaler : object from MinMaxScaler(), this maps the raw data and the [-1,1]
-              range
-  """
-  # Convert tubular data to 3-dimension time series data
-  num_sample = int(len(df)/seq_len)
-  time_dataset = []
-  for i in range(num_sample):
-    t_slice = df.iloc[(i*seq_len) : ((i+1)*seq_len)]
-    time_dataset.append(t_slice)
-
-  # Get the label of each sample
-  trainy= df['handlabeller_final'].values
-  encoder = OneHotEncoder(sparse=False)
-  OHE_y = encoder.fit_transform(trainy.reshape(-1, 1))
-  OHE_Y = OHE_y.reshape(num_sample, seq_len, -1)
-
-  # Get the data without label
-  to_drop = ['time', 'confidence', 'handlabeller1', 'handlabeller2', 'handlabeller_final', 'speed_1', 'direction_1', 'acceleration_1', 'speed_2', 'direction_2', 'acceleration_2', 'speed_4', 'direction_4', 'acceleration_4', 'speed_8', 'direction_8', 'acceleration_8', 'acceleration_16']
-  X= []
-  for sample in time_dataset :
-    features = sample.drop(to_drop, axis=1)
-    
-    #normalize using ppd
-    for col in features.columns:
-      features[col] /= ppd_value
-      
-    X.append(features)
-
-  # Convert the list of dataframe to numpy array and normalize/rescale it
-  concat_X = pd.concat(X, axis=0)
-  numpy_X = concat_X.values
+def preprocessing_data(df, seq_len, ppd):
   
-  #convert/change the shape of X to (num_sample, seq_len, feature)
-  X = numpy_X.reshape(num_sample, seq_len, -1)
-  #trainy = np.array(trainy)
-  print('The shape of time series data : ', X.shape)
-  print('The shape of time series data label : ', OHE_Y.shape)
-  return [X, OHE_Y]
+  #print length of df
+    print('The length of dataframe : ', len(df))
+    
+    # windowing data
+    num_sample = int(len(df)/seq_len)
+    time_dataset = []
+    for i in range(num_sample):
+        t_slice = df.iloc[(i*seq_len):((i+1)*seq_len)]
+        time_dataset.append(t_slice)
+    # print number of windowing data
+    print('The number of windowing data : ', len(time_dataset))
+    
+    to_drop = ['time', 'confidence', 'handlabeller1', 'handlabeller2', 'handlabeller_final', 'acceleration_1', 'acceleration_2', 'acceleration_4', 'acceleration_8', 'acceleration_16']
+    X = []
+    for sample in time_dataset:
+        features = sample.drop(to_drop, axis=1) # drop necessary features
+        for col in features.columns: # normalize features
+            features[col] /= ppd
+        X.append(features)
+    concat_X =pd.concat(X, axis=0)
+    np_X = concat_X.values
+    X = np_X.reshape(num_sample, seq_len, -1)
+    
+    #one hot encoding
+    df_label = df['handlabeller_final'].values
+    encoder = OneHotEncoder(sparse=False)
+    y_onehot = encoder.fit_transform(df_label.reshape(-1,1))
+    OHE_Y = y_onehot.reshape(num_sample, seq_len, -1)
+    
+    print('The shape of time series data: ', X.shape)
+    print('The shape of time series data label: ', OHE_Y.shape)
+    return [X, OHE_Y]
 
 def calculate_ppd(width_px, height_px, width_mm, height_mm, distance):
     theta_w = 2 * math.atan(width_mm / (2 * distance)) * 180. / math.pi
@@ -76,11 +60,11 @@ ppd = calculate_ppd(width_px, height_px, width_mm, height_mm, distance)
 print("ppd value: ", ppd)
 
 seq_len = 65
-df = pd.read_csv(r'C:\Users\Asus\Skripsi\MLPNAS_2\cnnblstm\DATA\merged_all_bridge2.csv')
+df = pd.read_csv(r'C:\Users\Asus\Skripsi\MLPNAS_2\cnnblstm\DATA\all_koenigstrasse_csv\merged_data_train_val_3.csv')
 # make length of df can be divided 
-df = df.iloc[:-8]
+df = df.iloc[:-61]
 
-dataset = load_data(df, seq_len,ppd)
+dataset = preprocessing_data(df, seq_len,ppd)
 x, y = dataset
 #print("cek isi x\n", x)
 #print("cek isi y\n", y)
